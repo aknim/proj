f9161e04c63721cea18fdb200ed7b4b4b2da2fbd
akshaya <akshayasunny@gmail.com>
2014-04-07 12:25:56 +0530

    images set

--- 2048/javascript.js
+++ 2048/javascript.js
@@ -1,16 +1,20 @@
 
 $(document).ready(function(){
-	$("td").html("&nbsp;");
+	$(".cell").html("&nbsp;");
 //	$("#11").html("<div class='d8'>abc</div>");
 	$("body").append("<div class='d8'>abc</div>");
 
 
 	$(document).on("click",".btn",function(){
 		clicked($(this).attr("id"));
-//		alert($(this).attr("id"));
 	});
 });
 
 function clicked(name){
-alert(name);
+chng="empty";dxn="empty";
+if ((name==="left")||(name==="right")) dxn="horizontal";
+else dxn="vertical";
+if ((name==="left")||(name==="down")) chng="negative";
+else chng="positive";
+console.log("img/btn:"name+" = "+chng+" "+dxn);
 }
