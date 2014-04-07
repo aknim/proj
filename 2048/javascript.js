13684413aacdab66b2c568b370024a90660aaae8
akshaya <akshayasunny@gmail.com>
2014-04-07 14:25:26 +0530

    collected horizontal values in arrays

--- 2048/javascript.js
+++ 2048/javascript.js
@@ -9,23 +9,67 @@ $(document).ready(function(){
 });
 
 function clicked(name){
-chng="empty";dxn="empty";
-if ((name==="left")||(name==="right")) dxn="horizontal";
-else dxn="vertical";
-if ((name==="left")||(name==="down")) chng="negative";
-else chng="positive";
-console.log("img/btn:"+name+" = "+chng+" "+dxn);
+orientation="empty";direction="empty";
+if ((name==="left")||(name==="right")) orientation="horizontal";
+else orientation="vertical";
+if ((name==="left")||(name==="down")) direction="negative";
+else direction="positive";
+console.log("img/btn:"+name+" = "+orientation+" "+direction);
+move(orientation,direction);
 }
+
+function move(orientation,direction)
+{
+	switch(orientation){
+	case 'horizontal' :
+		arr_h=[];
+		//get values in array
+		for(i=1;i<=4;i++){
+				arr=[];
+			for(j=1;j<=4;j++)
+			{
+				id="#"+i+""+j;//difference of horizontal or vertical
+				filled=($(id).hasClass("filled"));
+				if(!filled){}
+				else{arr.push($(id).html());}
+				
+			}
+			//console.log(arr);
+			arr_h.push(arr);
+		}
+		console.log(arr_h);	
+		break;
+	case 'vertical' :
+		break;
+
+	}
+}
+
 
 function start(){
 id1=getEmptyId();set(id1,"2");
 id2=getEmptyId();set(id2,"2");
+for(i=1;i<=4;i++)
+{
+	for(j=1;j<=4;j++)
+	{
+		set("#"+i+""+j,10*i+j);
+	}
+}
+unset("#44");
+unset("#33");
+unset("#12");unset("#13");
 }
 
+function unset(id){
+$(id).removeClass(); //removeall
+$(id).addClass("cell");
+$(id).html("&nbsp;");
+}
 function set(id,val){
 $(id).html(val);
-$(id).addClass("d"+val);$(id).addClass("filled");
-//$(id).addClass("filled");
+$(id).addClass("d"+val);
+$(id).addClass("filled");
 }
 
 function getEmptyId(){
